81eb3b8687135ee9731ba52dc6ed6d552222014f
lualcs <1508786776@qq.com>
2023-11-08 22:48:58 +0800

    优化上值

--- src/method/algor/expression.lua
+++ src/method/algor/expression.lua
@@ -1,5 +1,6 @@
+local load = load or loadstring
 local function localf(sexpr, ...)
-    local f = (load or loadstring)("return " .. sexpr:format(...))
+    local f = load("return " .. sexpr:format(...))
     return f()
 end
 

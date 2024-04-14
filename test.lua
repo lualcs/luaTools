d34db721064503331af7248ef1478dd046655969
lualcs <1508786776@qq.com>
2024-04-14 20:37:01 +0800

    还原

--- test.lua
+++ test.lua
@@ -1,3 +1,6 @@
+package.path = [[/usr/local/share/lua/5.4/?.lua;/usr/local/share/lua/5.4/?/init.lua;/usr/local/lib/lua/5.4/?.lua;/usr/local/lib/lua/5.4/?/init.lua;./?.lua;./?/init.lua]]
+package.cpath = [[/usr/local/lib/lua/5.4/?.so;/usr/local/lib/lua/5.4/loadall.so;./?.so]]
+
 local maskword = require("maskword")
 local arr2set = require("array.map")
 
@@ -9,3 +12,6 @@ local obj = maskword.new(arr2set {
 
 
 print(obj:conver("艹你妈"))
+
+
+
